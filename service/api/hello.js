bf3c9bfb0da662e96ca9362f238de295f3033307
Devin Rader <devin@twilio.com>
2014-06-06 11:48:29 -0400

    ordering

--- service/api/hello.js
+++ service/api/hello.js
@@ -1,6 +1,35 @@
 var twilio = require('twilio');
 
-exports.get = function(request, response) {
+exports.post = function (request, response) {
+
+    response.set('Content-Type', 'text/xml');
+
+    var resp = new twilio.TwimlResponse();
+    var employeesTable = request.service.tables.getTable('employees');
+    var callerid = request.param('From');
+    var digits = request.param('Digits');
+
+    employeesTable.where({
+        ID: digits
+    }).read({
+        success: function (results) {
+            if (results.length > 0) {
+                this.say('Your next shift begins on %s and ends %s');
+            } else {
+                console.log('I\'m sorry.  I could not find that I D', digits);
+                resp.hangup();
+            }
+
+            response.send(200, resp.toString());
+        },
+        error: function (err) {
+            console.error(err);
+            response.send(404, err);
+        }
+    });
+};
+
+exports.get = function (request, response) {
     
     response.set('Content-Type', 'text/xml');
 
@@ -33,31 +62,4 @@ exports.get = function(request, response) {
     });
 };
 
-exports.post = function (request, response) {
 
-    response.set('Content-Type', 'text/xml');
-
-    var resp = new twilio.TwimlResponse();
-    var employeesTable = request.service.tables.getTable('employees');
-    var callerid = request.param('From');
-    var digits = request.param('Digits');
-
-    employeesTable.where({
-        ID: digits
-    }).read({
-        success: function (results) {
-            if (results.length > 0) {
-                this.say('Your next shift begins on %s and ends %s');
-            } else {
-                console.log('I\'m sorry.  I could not find that I D', digits);
-                resp.hangup();
-            }
-
-            response.send(200, resp.toString());
-        },
-        error: function (err) {
-            console.error(err);
-            response.send(404, err);
-        }
-    });
-};
