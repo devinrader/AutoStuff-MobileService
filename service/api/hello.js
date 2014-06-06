b052bbb28ba2e49008a5170daef6cef34d88109e
Devin Rader <devin@twilio.com>
2014-06-06 11:34:21 -0400

    Adding initial gather handler.  Adding data table initializer script

--- service/api/hello.js
+++ service/api/hello.js
@@ -9,12 +9,12 @@ exports.get = function(request, response) {
     var callerid = request.param('From'); 
     
     employeesTable.where({
-        ID: callerid
+        phoneNumber: callerid
     }).read({
         success: function(results) {            
             if (results.length > 0) {                
                 resp.say('Thank you for calling the Auto Stuff employee schedule.');
-                resp.gather({ timeout:30, action: '' }, function() {
+                resp.gather({ timeout:30, method: 'POST' }, function() {
                     this.say('Please enter your employee ID');
                 });
             } else {
@@ -31,3 +31,31 @@ exports.get = function(request, response) {
     });
 };
 
+exports.post = function (request, response) {
+
+    response.set('Content-Type', 'text/xml');
+
+    var resp = new twilio.TwimlResponse();
+    var employeesTable = request.service.tables.getTable('employees');
+    var callerid = request.param('From');
+    var digits = request.param('Digits');
+
+    employeesTable.where({
+        ID: digits
+    }).read({
+        success: function (results) {
+            if (results.length > 0) {
+                this.say('Your next shift begins on %s and ends %s');
+            } else {
+                console.log('I\'m sorry.  I could not find that I D', digits);
+                resp.hangup();
+            }
+
+            response.send(200, resp.toString());
+        },
+        error: function (err) {
+            console.error(err);
+            response.send(404, err);
+        }
+    });
+};
---
+++ service/api/setup.js
@@ -0,0 +1,14 @@
+﻿exports.get = function (request, response) {
+
+    var employeesTable = request.service.tables.getTable('employees');
+
+    employeesTable.insert({phoneNumber: '+19102369959'}, {
+        success: function (results) {
+            response.send(200, resp.toString());
+        },
+        error: function (err) {
+            console.error(err);
+            response.send(500, err);
+        }
+    });
+};
